034dc78067089cf00f3687193c8b4b1707ee83e3
Yong Yang <yongyang.math@gmail.com>
2017-02-22 11:54:36 -0600

    A function to check whether a given integer is a power of 4.

---
+++ isPowerOfFour.py
@@ -0,0 +1,17 @@
+'''
+#  ======================================================================
+#     FileName: isPowerOfFour.py
+#      Project: Leetcode
+#       Author: Yong Yang, Department of Mathematics, UTA
+#        Email: yongyang@uta.edu; yongyang.math@gmail.com
+#      Created: 2017-02-22 11:52:06
+#   LastChange: 2017-02-22 11:52:16
+#  ======================================================================
+'''
+def isPowerOfFour(self, num):
+        """
+aGiven an integer (signed 32 bits), write a function to check whether it is a power of 4.
+        :type num: int
+        :rtype: bool
+        """
+        return num>0 and not(num&(num-1)) and len(bin(num))%2==1
